34e85c983e9f0b8051ac6728662390a51fc01849
Donald J. Olbris <olbrisd@janelia.hhmi.org>
2021-06-24 11:10:06 -0400

    added upload script

---
+++ util/upload.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# upload files to AWS S3; requires aws cli to be installed
+#   derived from script by Cristian
+
+STEP_SUBDIR=cdsresults.final
+DATA_DIR=/nrs/neuronbridge/v2
+UPLOAD_BUCKET=s3://janelia-neuronbridge-data-int
+VERSION_FOLDER=v2_1_1
+
+# echo for command preview, empty for actually doing it
+EXEC_CMD=echo
+
+# MIPs metadata uploads
+# MCFO lines
+# $EXEC_CMD aws s3 cp \
+#     ${DATA_DIR}/mips/gen1_mcfo_lines \
+#     ${UPLOAD_BUCKET}/${VERSION_FOLDER}/metadata/by_line --recursive
+# SplitGAL4 lines
+# $EXEC_CMD aws s3 cp \
+#     ${DATA_DIR}/mips/split_gal4_lines \
+#     ${UPLOAD_BUCKET}/${VERSION_FOLDER}/metadata/by_line --recursive
+# EM bodies
+# $EXEC_CMD aws s3 cp \
+#     ${DATA_DIR}/mips/em_bodies \
+#     ${UPLOAD_BUCKET}/${VERSION_FOLDER}/metadata/by_body --recursive
+
+# CDS results
+# $EXEC_CMD aws s3 cp \
+#     ${DATA_DIR}/${VERSION_FOLDER}/${STEP_SUBDIR}/flylight-vs-flyem \
+#     ${UPLOAD_BUCKET}/${VERSION_FOLDER}/metadata/cdsresults --recursive
+# $EXEC_CMD aws s3 cp \
+#     ${DATA_DIR}/${VERSION_FOLDER}/${STEP_SUBDIR}/flyem-vs-flylight \
+#     ${UPLOAD_BUCKET}/${VERSION_FOLDER}/metadata/cdsresults --recursive
+
+# metadata files
+$EXEC_CMD aws s3 cp \
+    ${DATA_DIR}/${VERSION_FOLDER}/DATA_VERSION \
+    ${UPLOAD_BUCKET}/${VERSION_FOLDER}/DATA_VERSION
+$EXEC_CMD aws s3 cp \
+    ${DATA_DIR}/${VERSION_FOLDER}/DATA_NOTES.md \
+    ${UPLOAD_BUCKET}/${VERSION_FOLDER}/DATA_NOTES.md
+$EXEC_CMD aws s3 cp \
+    ${DATA_DIR}/${VERSION_FOLDER}/publishedNames.txt \
+    ${UPLOAD_BUCKET}/${VERSION_FOLDER}/publishedNames.txt
+
+# do last; updating this file switchs to new results
+# $EXEC_CMD aws s3 cp \
+#     ${DATA_DIR}/${VERSION_FOLDER}/paths.json \
+#     ${UPLOAD_BUCKET}/paths.json
