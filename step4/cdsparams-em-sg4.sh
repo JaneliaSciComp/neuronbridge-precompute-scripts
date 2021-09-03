022ee644256e3e66b62044aca88c432d4eaf763f
Donald J. Olbris <olbrisd@janelia.hhmi.org>
2021-09-03 13:33:51 -0400

    corrected batching for step 4

--- step4/cdsparams-em-sg4.sh
+++ step4/cdsparams-em-sg4.sh
@@ -4,13 +4,24 @@
 
 
 # ------------------------------
-# same job partitioning as for step 3; source that params file for those value,
-#   then update the rest of the variables belos
+# user edited: job partitioning
 
-# if you need to adjust, copy the "job partition" section from that file
-#   and update it here, and do not source it
+# TOTAL_FILES is the least number > the number of files containing matches by EM that is divisible by FILES_PER_JOB
+# the reason for that is that in bash TOTAL_FILES/FILES_PER_JOB is an integer division and if it does not divide exactly
+# we may not process all the files
+# so to calculate it `ls ${CDSMATCHES_RESULTS_DIR}/${SG4_INPUT}-vs-${EM_INPUT} | wc` then take the least number > the value
+# that is divisible by the selected value for FILES_PER_JOB
+# the value depends on the CPU and memory resources available on the machine. If running on the grid requesting 20 cores
+export TOTAL_FILES=2400
+# for split gal4 drivers we can use up to 200 files per job - for MCFO we cannot go higher than 100 since the number of MCFOs
+# is much larger
+export FILES_PER_JOB=200
 
-source "$(dirname ${BASH_SOURCE[0]})/../step3/cdsparams-em-sg4.sh"
+export START_FILE_INDEX=0
+export TOTAL_JOBS=$(((TOTAL_FILES - START_FILE_INDEX) / FILES_PER_JOB + 1))
+
+# value should be smaller than for searches, perhaps 5-50 (vs 100-500 for searches)
+export PROCESSING_PARTITION_SIZE=50
 
 
 # ------------------------------
