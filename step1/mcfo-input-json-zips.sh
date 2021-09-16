853586ab0354fe3f6b09bd361636d4d1281d5343
Donald J. Olbris <olbrisd@janelia.hhmi.org>
2021-09-16 08:56:36 -0400

    added step 1 mcfo variant script with zip input

---
+++ step1/mcfo-input-json-zips.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# generate input JSON for MCFO dataset
+
+source "$(dirname ${BASH_SOURCE[0]})/../global-cdsparams.sh"
+
+LIBNAME=flylight_gen1_mcfo_published
+BASELIBDIR=/nrs/jacs/jacsData/filestore/system/ColorDepthMIPs/JRC2018_Unisex_20x_HR
+# LIBDIR=${BASELIBDIR}/flylight_gen1_mcfo_published
+LIBDIR=/nrs/scicompsoft/emlm/public_release_2021
+
+java ${JAVA_OPTS} \
+    -jar ${CDS_JAR} \
+    createColorDepthSearchJSONInput \
+    --jacsURL ${JACSV2URL} \
+    --authorization "Bearer ${JACSTOKEN}" \
+    -l ${LIBNAME} \
+    --librariesVariants \
+        ${LIBNAME}:searchable_neurons:${LIBDIR}/40x_MCFO_segmented_2021_04_CDM.zip \
+        ${LIBNAME}:gradient:${LIBDIR}/40xMCFO_segmented_2021_04_gradient_20px.zip \
+        ${LIBNAME}:zgap:${LIBDIR}/40x_MCFO_segmented_2021_04_RGB20px.zip \
+    --include-mips-without-publishing-name 2 \
+    --segmented-image-handling 2 \
+    --segmented-mips-variant searchable_neurons \
+    --segmentation-channel-base 1 \
+    --urls-relative-to 1 \
+    -od ${MIPS_DIR} \
+    --output-filename ${MCFO_INPUT}
