521fbd4ddbc6683fce85202d5fd638be4f94bad8
Donald J. Olbris <olbrisd@janelia.hhmi.org>
2021-09-20 13:52:13 -0400

    added new attribute update script

---
+++ util/update-attributes.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# update attributes: if you need to update attributes in any kind of search results file,
+#   but you don't need to rerun the searches (eg, for image URLs used for display on the site 
+#   only), use this script; the correct attributes should be in the aggregate json file
+#   produced by step 1 (the -attrs input); the output does not overwrite the input
+
+# this script will likely be edited for each case where it is needed; all the 
+#   usual location variables are available for use; note that the fileds names 
+#   must match the fields in the json files
+
+source "$(dirname ${BASH_SOURCE[0]})/../global-cdsparams.sh"
+
+
+java ${JAVA_OPTS} \
+    -jar ${CDS_JAR} \
+    replaceAttributes \
+    -attrs ${MIPS_DIR}/${EM_INPUT} \
+    --input-dirs ${CDS_FINAL_DIR}/"flylight-vs-flyem" \
+    --id-field id  \
+    --fields-toUpdate searchablePNG \
+    -od ${CDS_FINAL_DIR}/"flylight-vs-flyem-updated"
